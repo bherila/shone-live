b558fa9deac73af36e0e779f301171a6c1f11eae
Bretton Auerbach <bretton.auerbach@gmail.com>
2020-08-13 14:41:08 -0700

    add create & update dto for coffees
    - Creating a REST API application
      - Introduction to Data Transfer Objects

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,36 +7,38 @@ import {
     Patch,
     Post,
     Query,
-  } from '@nestjs/common';
-  import { CoffeesService } from './coffees.service';
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
-  @Controller('coffees')
-  export class CoffeesController {
-    constructor(private readonly coffeesService: CoffeesService) {}
+@Controller('coffees')
+export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) { }
 
     @Get()
     findAll(@Query() paginationQuery) {
-      // const { limit, offset } = paginationQuery;
-      return this.coffeesService.findAll();
+        // const { limit, offset } = paginationQuery;
+        return this.coffeesService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-      return this.coffeesService.findOne(id);
+        return this.coffeesService.findOne(id);
     }
 
     @Post()
-    create(@Body() body) {
-      return this.coffeesService.create(body);
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        return this.coffeesService.create(createCoffeeDto);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body) {
-      return this.coffeesService.update(id, body);
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCoffeeDto);
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
-      return this.coffeesService.remove(id);
+        return this.coffeesService.remove(id);
     }
-  }
+}
