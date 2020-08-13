90cda08e26f907e71c1d8fe4377a04d25f1e1b94
Bretton Auerbach <bretton.auerbach@gmail.com>
2020-08-13 14:21:12 -0700

    refactor coffees ctrl to use svc
    - Creating a REST API application
      - Creating a Basic Service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,39 +1,42 @@
 import {
+    Body,
     Controller,
+    Delete,
     Get,
     Param,
-    Post,
-    Body,
     Patch,
-    Delete,
-    Query
-} from '@nestjs/common';
+    Post,
+    Query,
+  } from '@nestjs/common';
+  import { CoffeesService } from './coffees.service';
 
-@Controller('coffees')
-export class CoffeesController {
+  @Controller('coffees')
+  export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) {}
+
     @Get()
     findAll(@Query() paginationQuery) {
-        const { limit, offset } = paginationQuery;
-        return `TODO: This action should return all coffees. Limit ${limit}, offset: ${offset}`;
+      // const { limit, offset } = paginationQuery;
+      return this.coffeesService.findAll();
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `TODO: This action should return coffee with id ${id}`;
+      return this.coffeesService.findOne(id);
     }
 
     @Post()
     create(@Body() body) {
-        return body;
+      return this.coffeesService.create(body);
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body) {
-        return `TODO: This action should update coffee with id ${id}`;
+      return this.coffeesService.update(id, body);
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return `TODO: This action should remove coffee with id ${id}`;
+      return this.coffeesService.remove(id);
     }
-}
+  }
