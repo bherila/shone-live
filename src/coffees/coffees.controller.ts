58b07a196c312ff69e090dd4272e330c36e27975
Bretton Auerbach <bretton.auerbach@gmail.com>
2020-08-13 13:40:15 -0700

    add find method to coffees controller
    - Creating a REST API application
      - Use Route Parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
     findAll() {
         return 'TODO: This action should return all coffees';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `TODO: This action should return coffee with id ${id}`;
     }
 }
