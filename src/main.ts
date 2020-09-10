4a1416570f8a6f72b66a3147c9be0f21bf250700
Bretton Auerbach <auerbachb@users.noreply.github.com>
2020-09-10 12:32:50 -0700

    [TOOLING] only log routes in dev (#18)
    wonder if there's a better way to manage the NODE_ENV throughout the app rather than `process.env.NODE_ENV`

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,10 @@ async function bootstrap() {
   );
   await app.listen(3000);
 
-  // todo, only run this in dev
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
-  console.log(expressListRoutes({}, 'API:', router));
+  if (process.env.NODE_ENV === 'dev') {
+    const server = app.getHttpServer();
+    const router = server._events.request._router;
+    console.log(expressListRoutes({}, 'API:', router));
+  }
 }
 bootstrap();
