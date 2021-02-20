736fb60f7f77d73adf6297ae2442ad8d8afaf3da
Najam <najamiftikhar2020@gmail.com>
2021-02-20 12:20:08 +0500

    work on db

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,6 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { User } from '../../entities/User'
+
 const handler = (req: NextApiRequest, res: NextApiResponse): any => {
+  createConnection({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'najam',
+    entities: [User],
+    synchronize: true,
+    logging: false,
+    port: 3306,
+  })
+    .then((connection) => {
+      console.log('CONNECTEDDDDDDDDDDDDDDDD')
+    })
+    .catch((error) => console.log(error))
   res.status(200).json(req.body)
 }
 
