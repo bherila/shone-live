edcae230b91ca948ed9662400d80713da1dd35bb
Najam <najamiftikhar2020@gmail.com>
2021-02-20 19:35:58 +0500

    work on db

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,21 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import 'reflect-metadata'
-import Connection from '../../lib/connection';
+import Connection from '../../lib/connection'
 
-async function handler(req: NextApiRequest, res: NextApiResponse):Promise<void> {
-    try {
-        if (await Connection) {
-            console.info('CONNECTEDDDDDDDDDDDDDDDD!!')
-            res.status(200).json(req.body)
-        }
-        else {
-            throw new Error("No connection :(")
-        }
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  try {
+    if (await Connection) {
+      console.info('connected...')
+      res.status(200).json(req.body)
+    } else {
+      throw new Error('No connection :(')
     }
-    catch (err) {
-        res.status(500).json({error: err});
-    }
+  } catch (err) {
+    res.status(500).json({ error: err })
+  }
 }
 
 export default handler
