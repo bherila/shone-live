3ed3a1022668bb701063fbf9122c3c4a50c8c488
Najam <najamiftikhar2020@gmail.com>
2021-02-23 23:51:30 +0500

    refactor

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,6 @@
-import { useEffect } from 'react'
 import Navbar from '../Navbar/Navbar'
 
 const Layout = ({ children }): JSX.Element => {
-  useEffect(() => {}, [])
-
   return (
     <>
       <Navbar />
