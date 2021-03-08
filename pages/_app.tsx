e8ce7ecb53e60957baacce410cb2840687116ec5
Ben Herila <ben@herila.net>
2021-03-07 23:10:32 -0700

    Password protection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import "tailwindcss/tailwind.css";
-
+import App from "next/app"
+import Cookies from "universal-cookie"
+import consts from "../components/consts"
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
+}
+
+MyApp.getInitialProps = async (appContext) => {
+    const appProps = await App.getInitialProps(appContext)
+    const cookies = new Cookies(appContext.ctx.req.headers.cookie)
+    const password = cookies.get(consts.SiteReadCookie) ?? ""
+    if (password === "letmein") {
+        appProps.pageProps.hasReadPermission = true
+    }
+    return {...appProps}
 }
 
 export default MyApp
