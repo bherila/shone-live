bfeb56847e01c725a40dcc4d315812c721a8064c
Ben Herila <ben@herila.net>
2021-03-08 21:22:51 -0700

    Update footer with correct address

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,22 @@ import {
   FaLinkedinIn,
   FaPinterestP,
 } from 'react-icons/fa'
+import Image from 'next/image'
+
+function Icon({children}) {
+  return (
+      <div className="rounded-full flex items-center justify-center... border" style={{height:40,width:40,justifyContent:"center",alignItems:"center",margin:5}}>
+        {children}
+      </div>
+  )
+}
+
+const AddressLine = ({children}) => (
+    <div style={{ color: '#222d39' }} className="font-medium pt-2">
+      {children}
+    </div>
+)
+
 export default function Footer() {
   const [isExpanded, toggleExpansion] = useState(false)
   const [navLinks, setNavLinks] = useState([
@@ -21,7 +37,7 @@ export default function Footer() {
       <div className="container mx-auto">
         <div className="flex grid  sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-4 gap-1 ">
           <div className="flex-2">
-            <img src="/logo.png" width="140" height="40" />
+            <Image src="/logo.png" width={140} height={70} /><br />
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium dolor emque laudantium.
           </div>
@@ -51,15 +67,9 @@ export default function Footer() {
           </div>
           <div className="flex-1 ...">
             <div className="font-bold">Contact Us</div>
-            <div style={{ color: '#222d39' }} className="font-medium pt-2">
-              +088 123 456
-            </div>
-            <div style={{ color: '#222d39' }} className="font-medium pt-2">
-              shone@example.com
-            </div>
-            <div style={{ color: '#222d39' }} className="font-medium pt-2">
-              Ab raod, Dhaka
-            </div>
+            <AddressLine>Shone Live Co.</AddressLine>
+            <AddressLine>99 Rausch St. Unit 526</AddressLine>
+            <AddressLine>San Francisco, CA 94103</AddressLine>
           </div>
         </div>
         <br />
@@ -80,23 +90,12 @@ export default function Footer() {
             }}
             className="lg:flex-1 sm:flex-0"
           >
-            <div className="rounded-full flex items-center justify-center... border" style={{height:40,width:40,justifyContent:"center",alignItems:"center",margin:5}}>
-              <FaFacebookF size={20} color="#19B5FE" />
-            </div>
+            <Icon><FaFacebookF size={20} color="#19B5FE" /></Icon>
 
             {/* <div className="px-2 w-8 h-8 border-2 rounded-full"></div> */}
-
-            <div className="rounded-full flex items-center justify-center... border" style={{height:40,width:40,justifyContent:"center",alignItems:"center",margin:5}}>
-              <FaTwitter size={20} color="#fff" />
-            </div>
-
-            <div className="rounded-full flex items-center justify-center... border" style={{height:40,width:40,justifyContent:"center",alignItems:"center",margin:5}}>
-              <FaLinkedinIn size={20} color="#fff" />
-            </div>
-
-            <div className="rounded-full flex items-center justify-center... border" style={{height:40,width:40,justifyContent:"center",alignItems:"center",margin:5}}>
-              <FaPinterestP size={20} color="#fff" />
-            </div>
+            <Icon><FaTwitter size={20} color="#fff" /></Icon>
+            <Icon><FaLinkedinIn size={20} color="#fff" /></Icon>
+            <Icon><FaPinterestP size={20} color="#fff" /></Icon>
           </div>
         </nav>
       </div>
