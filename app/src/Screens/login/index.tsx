b7342fffb35d102acc978827d7aed57b4df5386f
bipinsakariya <bipinskariya002@gmail.com>
2021-03-26 06:09:50 -0700

    #44 open keyboard by default on otp screen

--- app/src/Screens/login/index.tsx
+++ app/src/Screens/login/index.tsx
@@ -61,13 +61,14 @@ export default function Login() {
                 <TextInputMask
                   style={styles._input}
                   type={'cel-phone'}
+                  autoFocus={true}
                   options={{
                     maskType: 'BRL', // for international set it -&amp;nbsp;INTERNATIONAL type masking
                     withDDD: true,
-                    dddMask: '999-999-9999', // this is a your define formatting you use according to your requirment
+                    dddMask: '999-999-9999' // this is a your define formatting you use according to your requirment
                   }}
                   maxLength={12} // set length according to your input requirment
-                  onChangeText={(text) => {
+                  onChangeText={text => {
                     setMobile(text)
                   }}
                   value={mobile}
