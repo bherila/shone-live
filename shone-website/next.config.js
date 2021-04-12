92f3307bec4c88e03a8bdf86272391eb83fabb6c
Facundo Castello <facundoacastello@gmail.com>
2021-04-12 16:19:13 -0400

    add thanks page

--- shone-website/next.config.js
+++ shone-website/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
   env: {
     APOLLO_URI: process.env.APOLLO_URI,
   },
--- shone-website/src/pages/index.tsx
+++ shone-website/src/pages/index.tsx
@@ -1,23 +1,36 @@
 import cn from 'classnames'
 import Head from 'next/head'
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
+import Tick from '../../public/tick.svg'
 import AnimatedLogo from '../components/animated-logo'
 import rainbow from '../components/RainbowGradientContainer.module.css'
 import { useSubscribeToWaitlistMutation } from '../generated/graphql'
+import isValidEmail from '../helpers/isValidEmail'
+import useInput from '../hooks/useInput'
 
 export default function Index(): JSX.Element {
-  const [email, setEmail] = useState('')
-  const [createConsumerLead] = useSubscribeToWaitlistMutation()
+  const [submitted, setSubmitted] = useState(false)
+  const { value: email, setValue: setEmail, valid } = useInput({
+    defaultValue: '',
+    validate: isValidEmail,
+  })
+  const [
+    createConsumerLead,
+    { loading, data },
+  ] = useSubscribeToWaitlistMutation()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setSubmitted(true)
+    if (!valid || loading) return
     try {
       await createConsumerLead({
         variables: { email },
       })
-    } catch (error) {}
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -36,31 +49,52 @@ export default function Index(): JSX.Element {
             <AnimatedLogo />
           </div>
           <div className="">
-            <form className="text-center flex flex-col w-full">
-              <h1 className="text-4xl uppercase py-4">Be the first to know</h1>
-              <p className="py-4">
-                We’ll send you the link to download SHONE when it’s your turn.
-              </p>
-              <input
-                autoFocus={true}
-                placeholder="email"
-                className="px-4 py-2 my-4 rounded-full border-gray-50 w-1/2 mx-auto"
-                style={{
-                  background: '#E9E9E9',
-                }}
-                type="email"
-                onChange={(e) => setEmail(e.currentTarget.value)}
-                value={email}
-              />
-              <button
-                type={'submit'}
-                onClick={handleSubmit}
-                className="rounded-full bg-orange-400 uppercase py-2 px-4 text-white mx-auto flex-grow-0"
-                style={{ background: '#FCB344' }}
-              >
-                Join the waitlist
-              </button>
-            </form>
+            {!data ? (
+              <form className="text-center flex flex-col w-full">
+                <h1 className="text-4xl uppercase py-4">
+                  Be the first to know
+                </h1>
+                <p className="py-4">
+                  We’ll send you the link to download SHONE when it’s your turn.
+                </p>
+                <div className="my-4">
+                  <input
+                    autoFocus={true}
+                    placeholder="email"
+                    className="px-4 py-2 rounded-full border-gray-50 w-1/2 mx-auto"
+                    style={{
+                      background: '#E9E9E9',
+                    }}
+                    type="email"
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                    value={email}
+                  />
+                  {submitted && !valid && (
+                    <div className="text-xs font-light text-red-500">
+                      Invalid email
+                    </div>
+                  )}
+                </div>
+                <button
+                  disabled={loading}
+                  type={'submit'}
+                  onClick={handleSubmit}
+                  className="rounded-full bg-orange-400 uppercase py-2 px-4 text-white mx-auto flex-grow-0"
+                  style={{ background: '#FCB344' }}
+                >
+                  Join the waitlist
+                </button>
+              </form>
+            ) : (
+              <div>
+                <div className="flex flex-col items-center">
+                  <div className="text-3xl w-8/12 text-center">
+                    THANK YOU FOR SENDING YOUR EMAIL!
+                  </div>
+                  <Tick className="mt-4 w-1/12" />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
