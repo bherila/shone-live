de2830654fdcf0cefd23ea052bbbc532075fa3f0
Riddhi Tagline <riddhi.tagline@gmail.com>
2021-04-22 10:10:06 +0530

    rename file

---
+++ shone-website/src/nest/user/dto/update-user.dto.ts
@@ -0,0 +1,21 @@
+import { Field, ID, InputType, ObjectType } from '@nestjs/graphql'
+import { GraphQLUpload } from 'apollo-server-express'
+import { IsOptional } from 'class-validator'
+import { FileUpload } from 'graphql-upload'
+
+@InputType()
+export class updateUserEntityDto {
+  id: string
+
+  @IsOptional()
+  @Field({ nullable: true })
+  username: string
+
+  @IsOptional()
+  @Field({ nullable: true })
+  email: string
+
+  @IsOptional()
+  @Field(() => GraphQLUpload, { nullable: true })
+  file: FileUpload
+}
